1fb391771eee8f24829d7ba61ba518a99bbb1936
Iheb ZARGOUNI <iheb.zargouni.pr@gmail.com>
2023-11-15 15:11:46 +0100

    second commit

--- Config/ConnectDb.js
+++ Config/ConnectDb.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const ConnectDb = async () => {
     try {
-        await mongoose.connect('mongodb+srv://ihebzargounipr:07112023@cluster0.jjgehee.mongodb.net/?retryWrites=true&w=majority');
+        await mongoose.connect('mongodb+srv://**********:********@cluster0.jjgehee.mongodb.net/?retryWrites=true&w=majority');
         console.log('You are connected to your database');
     } catch (err) {
         console.log(err)
